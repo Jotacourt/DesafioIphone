1d5be2c441eb1618ad6b179df2c5ed7d2e8e2366
Jotacourt <julio16bittencourt@gmail.com>
2024-05-23 13:54:05 -0300

    Configuração Iphone

---
+++ src/AparelhoTelefonico.java
@@ -0,0 +1,13 @@
+public class AparelhoTelefonico {
+    public static void ligar() {
+        System.out.println("Ligando para alguém...");
+    }
+
+    public static void atender() {
+        System.out.println("Atendendo a ligação...");
+    }
+
+    public static void iniciarCorreioVoz() {
+        System.out.println("Iniciando correio de voz...");
+    }
+}
---
+++ src/Iphone.java
@@ -0,0 +1,25 @@
+public class Iphone {
+    public void tocar() {
+        System.out.println("Tocando música...");
+    }
+
+    public void pausar() {
+        System.out.println("Música pausada.");
+    }
+
+    public void selecionarMusica() {
+        System.out.println("Selecionando uma música...");
+    }
+
+    public void exibirPagina(String pagina) {
+        System.out.println("Exibindo página: " + pagina);
+    }
+
+    public void adicionarNovaAba() {
+        System.out.println("Adicionando nova aba...");
+    }
+
+    public void atualizarPagina() {
+        System.out.println("Atualizando página...");
+    }
+}
